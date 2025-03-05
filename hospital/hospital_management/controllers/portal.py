82b973cb7183211142cdc60aa8f76e7395cb134e
Amisha1802 <amishasharma181102@gmail.com>
2025-03-05 17:37:30 +0530

    MODIFIED [commit]

--- hospital/hospital_management/controllers/portal.py
+++ hospital/hospital_management/controllers/portal.py
@@ -8,18 +8,31 @@ class AppointmentPortal(CustomerPortal):
     def _prepare_home_portal_values(self, counters):
         print("HOSPITAL_MANAGEMENT", counters)
         values = super()._prepare_home_portal_values(counters)
+        print("=2222222==", request.env['hospital.appointment'].check_access_rights('read', raise_exception=False))
         if 'appointment_count' in counters:
-            values['appointment_count'] = request.env['hospital.appointment'].sudo().search_count([
+            values['appointment_count'] = request.env['hospital.appointment'].search_count([
                 ('patient_id', '=', request.env.user.partner_id.id)
             ])
         return values
-        # if 'project_count' in counters:
-        #     values['project_count'] = request.env['project.project'].search_count([]) \
-        #         if request.env['project.project'].check_access_rights('read', raise_exception=False) else 0
-        # if 'task_count' in counters:
-        #     values['task_count'] = request.env['project.task'].search_count([('project_id', '!=', False)]) \
-        #         if request.env['project.task'].check_access_rights('read', raise_exception=False) else 0
-        # return values
+
+    @http.route(['/my/appointments'], type='http', auth="user", website=True)
+    def portal_my_appointments(self, **kwargs):
+        print("PORTAL", self, kwargs)
+        appointments = request.env['hospital.appointment'].sudo().search([
+            ('patient_id', '=', request.env.user.patient_id.id)
+        ])
+        return request.render('hospital_management.portal_appointment_template', {
+            'appointments': appointments,
+        })
+
+    @http.route(['/my/appointments/<int:appointment_id>'], type='http', auth="user", website=True)
+    def portal_appointment_detail(self, ticket_id, **kwargs):
+        appointment = request.env['hospital.appointment'].sudo().browse(appointment_id)
+        if not appointment.exists():
+            return request.not_found()
+        return request.render('hospital_management.portal_my_appointments', {
+            'appointment': appointment,
+        })
 
     def _prepare_portal_layout_values(self):
         """Prepares layout values for the portal."""
